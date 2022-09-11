0e26f68af53aeeb3497b1fe5c592634ef9f2f11c
id39 <100359834+idme520@users.noreply.github.com>
2022-09-11 10:39:05 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,69 @@
+let a = document.getElementById("git");
+let b = document.getElementById("chun");
+let c = document.getElementById("zhong");
+let d = document.getElementById("guo");
+let e = document.getElementById("bbb");
+let f = document.getElementById("ccc");
+let g = document.getElementById("ddd");
+let h = document.getElementById("eee");
+let i = document.getElementById("aaa");
+let j = document.getElementById("js2");
+function git() {
+  if (bbb.style.display === "none") {
+    bbb.style.display = "block";
+    ccc.style.display = "none";
+    ddd.style.display = "none";
+    eee.style.display = "none";
+  } else {
+    bbb.style.display = "none";
+    ccc.style.display = "none";
+    ddd.style.display = "none";
+    eee.style.display = "none";
+  }
+}
+function chun() {
+  if (ddd.style.display === "none") {
+    ddd.style.display = "block";
+    ccc.style.display = "none";
+    bbb.style.display = "none";
+    eee.style.display = "none";
+  } else {
+    bbb.style.display = "none";
+    ccc.style.display = "none";
+    ddd.style.display = "none";
+    eee.style.display = "none";
+  }
+}
+function zhong() {
+  if (eee.style.display === "none") {
+    eee.style.display = "block";
+    ccc.style.display = "none";
+    ddd.style.display = "none";
+    bbb.style.display = "none";
+  } else {
+    bbb.style.display = "none";
+    ccc.style.display = "none";
+    ddd.style.display = "none";
+    eee.style.display = "none";
+  }
+}
+function guo() {
+  if (ccc.style.display === "none") {
+    ccc.style.display = "block";
+    bbb.style.display = "none";
+    ddd.style.display = "none";
+    eee.style.display = "none";
+  } else {
+    bbb.style.display = "none";
+    ccc.style.display = "none";
+    ddd.style.display = "none";
+    eee.style.display = "none";
+  }
+}
+function js2() {
+  if (aaa.style.display === "none") {
+    aaa.style.display = "block";
+  } else {
+    aaa.style.display = "none";
+  }
+}
